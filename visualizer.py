664bcc54c64fcaafc8479dec84326225652a37b1
AkIonSight <akionsight@gmail.com>
2022-08-07 19:47:52 +0530

    Add the visualizer

---
+++ visualizer.py
@@ -0,0 +1,34 @@
+from matplotlib import pyplot as plt
+import math
+
+
+def three_x_plus_one(num):
+
+    steps = [num]
+
+    while (num > 1):
+
+        if num % 2 == 0:
+            num = num / 2
+            steps.append(num)
+            continue
+
+        else:
+            num = 3 * num + 1
+            steps.append(num)
+            continue
+    
+    return steps
+
+steps = three_x_plus_one(7225)
+
+print(steps)
+
+plt.plot(steps, color="magenta")
+plt.plot()
+
+
+plt.xlabel(f"Number of Steps: {len(steps)}")
+plt.ylabel(f"Values Achieved: {max(steps)}")
+
+plt.show()
